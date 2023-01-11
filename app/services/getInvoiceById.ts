ccf835b14a7758df8576680b7fa4e4183402e6e3
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:40:09 -0300

    feat: create a service geeInvoiceById

---
+++ app/services/getInvoiceById.ts
@@ -0,0 +1,13 @@
+import { Invoice } from "@/app/types";
+import api from "@/lib/axios";
+
+type GetInvoiceByIdProps = {
+  invoiceId: string;
+};
+
+export default async function getInvoiceById({
+  invoiceId,
+}: GetInvoiceByIdProps): Promise<Invoice> {
+  const res = await api.get(`/invoice/${invoiceId}`);
+  return res.data;
+}
