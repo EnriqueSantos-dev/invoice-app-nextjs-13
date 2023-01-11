e9af4588924d577e54b31c06e34994ee4fb7207c
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:50:56 -0300

    feat: created mapper-values-form-to-persistence util

---
+++ app/utils/mapper-values-form-to-persistence.ts
@@ -0,0 +1,43 @@
+import {
+  FormValues,
+  InvoiceItemPlusIdRandom,
+  StatusInvoice,
+} from "@/app/types";
+
+type MapperValuesFormToPersistenceProps = {
+  values: {
+    shortId?: string;
+    items: Partial<InvoiceItemPlusIdRandom>[];
+  } & Omit<FormValues, "items">;
+  status: StatusInvoice;
+};
+
+export default function mapperValuesFormToPersistence({
+  values,
+  status,
+}: MapperValuesFormToPersistenceProps) {
+  return {
+    shortId: values.shortId,
+    customer: {
+      city: values.billToCity,
+      streetAddress: values.billToStreetAddress,
+      country: values.billToCountry,
+      email: values.billToEmail,
+      name: values.billToName,
+      zipCode: values.billToPostCode,
+    },
+    ownerAddress: {
+      city: values.billFromCity,
+      streetAddress: values.billFromStreetAddress,
+      country: values.billFromCountry,
+      zipCode: values.billFromPostCode,
+    },
+    description: values.description,
+    items: values.items.map((item) => {
+      const { uuid, ...rest } = item;
+      return rest;
+    }),
+    status: status,
+    paymentDate: values.invoiceDate,
+  };
+}
