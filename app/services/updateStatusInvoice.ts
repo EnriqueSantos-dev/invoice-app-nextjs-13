7b61c1686bd783005268ecd0711e645d87e8f1d9
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:41:13 -0300

    feat: create a service updateStatusInvoice

---
+++ app/services/updateStatusInvoice.ts
@@ -0,0 +1,16 @@
+import { StatusInvoice } from "@/app/types";
+import api from "@/lib/axios";
+
+type UpdateStatusProps = {
+  invoiceId: string;
+  status: StatusInvoice;
+};
+
+export default async function updateInvoiceStatus({
+  invoiceId,
+  status,
+}: UpdateStatusProps): Promise<void> {
+  await api.patch(`/invoice/${invoiceId}/changeStatus`, {
+    status,
+  });
+}
