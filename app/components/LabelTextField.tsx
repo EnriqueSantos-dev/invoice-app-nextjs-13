223580775a69214ee93e78ec5a065b5b1eac423f
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 10:03:17 -0300

    feat: create LabelTextFieldComponent

---
+++ app/components/LabelTextField.tsx
@@ -0,0 +1,26 @@
+type LabelTextFieldProps = {
+  errorMessage?: string;
+  children: React.ReactNode;
+  label: string;
+  isRequiredField?: boolean;
+};
+
+export default function LabelTextField({
+  errorMessage,
+  label,
+  isRequiredField = false,
+  children,
+}: LabelTextFieldProps) {
+  return (
+    <label className="flex w-full cursor-pointer flex-col gap-3 whitespace-nowrap">
+      <div className="flex items-center gap-3 text-sm md:justify-between md:gap-0">
+        <p className="capitalize text-shipCove dark:text-white">
+          {label}
+          {!isRequiredField && " (optional)"}
+        </p>
+        <p className="text-burntSienna">{errorMessage}</p>
+      </div>
+      {children}
+    </label>
+  );
+}
