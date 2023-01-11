7573763cc02a9b40e36b7765632686bf43bf5fd6
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 15:08:56 -0300

    refactor: add correct path baseUrl to development mode

--- app/lib/axios/index.ts
+++ app/lib/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let url =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3000/api";
 
 const api = axios.create({
   baseURL: url,
