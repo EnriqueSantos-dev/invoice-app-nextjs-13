f25ddb5f60a154ae8f2b2d4b66700d6aceda640c
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:39:52 -0300

    feat: create a service deleteInvoice

---
+++ app/services/deleteInvoice.ts
@@ -0,0 +1,9 @@
+import api from "../lib/axios";
+
+export default async function deleteInvoice({
+  invoiceId,
+}: {
+  invoiceId: string;
+}): Promise<void> {
+  await api.delete(`/invoice/${invoiceId}/delete`);
+}
