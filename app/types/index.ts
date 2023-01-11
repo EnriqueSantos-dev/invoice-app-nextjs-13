cfb56ae0732e3306f4cc9742b4bcedf9a1f2ba12
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:43:40 -0300

    chore: all types used in app

---
+++ app/types/index.ts
@@ -0,0 +1,69 @@
+export interface Invoice {
+  id: string;
+  shortId: string;
+  status: StatusInvoice;
+  description: string;
+  paymentDate: string;
+  createdAt: string;
+  ownerAddress: Address;
+  customer: Customer;
+  items: InvoiceItem[];
+}
+
+export interface InvoiceItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export interface InvoiceItemPlusIdRandom extends InvoiceItem {
+  uuid: string;
+}
+
+export const Status = {
+  DRAFT: "DRAFT",
+  PENDING: "PENDING",
+  PAID: "PAID",
+} as const;
+
+export type StatusInvoice = (typeof Status)[keyof typeof Status];
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  invoices: Invoice[];
+}
+
+export interface Address {
+  id?: number;
+  streetAddress: string;
+  zipCode: string;
+  country: string;
+  city: string;
+}
+
+export interface Customer {
+  id?: string;
+  name: string;
+  email: string;
+  address: Address;
+}
+
+export type FormValues = {
+  billFromStreetAddress: string;
+  billFromPostCode: string;
+  billFromCountry: string;
+  billFromCity: string;
+  billToName: string;
+  billToEmail: string;
+  billToStreetAddress: string;
+  billToPostCode: string;
+  billToCountry: string;
+  billToCity: string;
+  invoiceDate: string;
+  description: string;
+  items: InvoiceItemPlusIdRandom[];
+};
---
+++ app/types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      name: string;
+      image: string;
+    };
+  }
+}
