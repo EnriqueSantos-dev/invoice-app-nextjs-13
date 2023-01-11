19c09af1896dd3ea2aa1355fc02f11bdda891903
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:50:08 -0300

    feat: created get-payment-date-formatted util

---
+++ app/utils/get-payment-date-formatted.ts
@@ -0,0 +1,13 @@
+export default function getPaymentDateFormatted(date: Date | string) {
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  };
+
+  const dateFormatted = new Intl.DateTimeFormat("en-US", options)
+    .format(typeof date === "string" ? new Date(date) : date)
+    .replace(/\,/gi, " ");
+
+  return "Due " + dateFormatted;
+}
