60560b22e7528dabe1a0e900b2c80821673a759b
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 10:11:47 -0300

    feat: create next-auth config

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session({ session, user }) {
+      if (user && session) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
