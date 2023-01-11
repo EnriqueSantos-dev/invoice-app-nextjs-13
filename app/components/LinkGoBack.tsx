e1dc86edfebca52fe7ef45bf8b00d29ea41ff432
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 10:08:07 -0300

    feat: create LinkGoBack component

---
+++ app/components/LinkGoBack.tsx
@@ -0,0 +1,16 @@
+import Link, { LinkProps } from "next/link";
+import { BiChevronLeft } from "react-icons/bi";
+
+interface LinkGoBackProps extends LinkProps {}
+
+export default function LinkGoBack(props: LinkGoBackProps) {
+  return (
+    <Link
+      className="flex items-center gap-3 text-sm font-bold text-black dark:text-white md:text-base"
+      {...props}
+    >
+      <BiChevronLeft className="h-4 w-4 fill-purple font-bold" />
+      Go back
+    </Link>
+  );
+}
