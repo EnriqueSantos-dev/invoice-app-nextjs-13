fc1eda838d80d94626f22ba58f8a70847ae77029
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 10:06:35 -0300

    feat: create component Providers as a wrapper to encapsulate all app providers

---
+++ app/components/Providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { queryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Providers({ children }: Props) {
+  return (
+    <>
+      <ToastContainer />
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+      </SessionProvider>
+    </>
+  );
+}
