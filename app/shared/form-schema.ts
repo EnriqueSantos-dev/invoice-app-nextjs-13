46aea708ad8b1bbac7d704c12d1bcc475b6e115d
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:41:38 -0300

    feat: create formSchema for formInvoice component

---
+++ app/shared/form-schema.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+const formSchema = z.object({
+  billFromStreetAddress: z.string().min(1, { message: "cannot be empty" }),
+  billFromPostCode: z
+    .string()
+    .trim()
+    .min(1, { message: "cannot be empty" })
+    .regex(/^\d{5}(?:[-\s]\d{4})?$/),
+  billFromCity: z.string().min(1, { message: "cannot be empty" }),
+  billFromCountry: z.string().min(1, { message: "cannot be empty" }),
+  billToName: z.string().min(1, { message: "cannot be empty" }),
+  billToEmail: z
+    .string()
+    .email("this email is not valid")
+    .min(1, { message: "cannot be empty" })
+    .transform((value) => value.toLowerCase()),
+  billToStreetAddress: z.string().min(1, { message: "cannot be empty" }),
+  billToPostCode: z
+    .string()
+    .trim()
+    .min(1, { message: "cannot be empty" })
+    .regex(/^\d{5}(?:[-\s]\d{4})?$/, "invalid format"),
+  billToCountry: z.string().min(1, { message: "cannot be empty" }),
+  billToCity: z.string().min(1, { message: "cannot be empty" }),
+  invoiceDate: z
+    .string()
+    .min(1, { message: "cannot be empty" })
+    .transform((value) => new Date(value)),
+  description: z.string().min(1, { message: "cannot be empty" }),
+});
+
+export default formSchema;
