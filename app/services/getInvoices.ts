75bc433d8a98dd9580eaa396ecae99bc0f42bb98
Enrique Santos <santosenrique869@gmail.com>
2023-01-11 11:40:22 -0300

    feat: create a service geeInvoices

---
+++ app/services/getInvoices.ts
@@ -0,0 +1,21 @@
+import { Invoice, StatusInvoice } from "@/app/types";
+import api from "@/lib/axios";
+
+type GetInvoicesProps = {
+  cursor?: string | null;
+};
+
+export type GetInvoicesResponse = {
+  nextCursor: string | null | undefined;
+  invoices: Invoice[];
+};
+
+export default async function getInvoices({
+  cursor,
+}: GetInvoicesProps): Promise<GetInvoicesResponse> {
+  const res = await api.get("/invoices", {
+    params: { cursor },
+  });
+  const data = res.data;
+  return data;
+}
