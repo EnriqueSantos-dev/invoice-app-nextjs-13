f4a3a4948c18b0ad9572dc3394d04d4c1ec97e37
enrique <enrique@pop-os.localdomain>
2023-01-13 12:38:59 -0300

    style: add font-family in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import "@/app/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import Providers from "./components/Providers";
+import { League_Spartan } from "@next/font/google";
+
+const roboto = League_Spartan({
+  weight: ["400", "500", "700"],
+  adjustFontFallback: true,
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <head />
       <body>
         <Providers>{children}</Providers>
