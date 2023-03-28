4cf944c6cc00380e4d1d78698ac41d6725c412b5
Enrique Santos <santosenrique869@gmail.com>
2023-03-28 15:32:48 -0300

    refactor: send user to previous url in search params from

--- app/components/AuthButtons.tsx
+++ app/components/AuthButtons.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 import { BsGithub } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 
 export function AuthButtons() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams?.get("from") ?? "/";
+
   return (
     <div className="w-full space-y-4 mt-12">
       <button
         className="rounded-lg w-full py-4 font-bold bg-black text-white items-stretch capitalize flex  gap-3 justify-center hover:opacity-90 transition-opacity"
-        onClick={async () => await signIn("github", { callbackUrl: `/` })}
+        onClick={() => signIn("github", { callbackUrl })}
       >
         <BsGithub className="w-5 h-5" />
         Sign In With Github
@@ -17,7 +21,7 @@ export function AuthButtons() {
 
       <button
         className="rounded-lg w-full py-4 font-bold bg-[#eee] text-black items-center capitalize flex  gap-3 justify-center hover:opacity-90 transition-opacity"
-        onClick={async () => await signIn("google", { callbackUrl: `/` })}
+        onClick={() => signIn("google", { callbackUrl })}
       >
         <FcGoogle className="w-5 h-5" />
         Sign In Google
