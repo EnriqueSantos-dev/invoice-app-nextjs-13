0b7dd552920ac5764d35d35ef71a3f348452fa30
Enrique Santos <santosenrique869@gmail.com>
2023-04-18 13:13:45 -0300

    feat: validate environment variables with zod

---
+++ app/types/env.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import * as dotenv from "dotenv";
+import * as dotenvExpand from "dotenv-expand";
+import { z } from "zod";
+
+const envObj = dotenv.config();
+dotenvExpand.expand(envObj);
+
+export const envSchema = z.object({
+	GITHUB_CLIENT_ID: z.string(),
+	GITHUB_CLIENT_SECRET: z.string(),
+	GOOGLE_CLIENT_ID: z.string(),
+	GOOGLE_CLIENT_SECRET: z.string(),
+	DATABASE_URL: z.string(),
+	NEXTAUTH_URL: z.string(),
+	NEXTAUTH_SECRET: z.string(),
+	POSTGRES_DB: z.optional(z.string()),
+	POSTGRES_PASSWORD: z.optional(z.string()),
+	POSTGRES_PORT: z.optional(z.coerce.number()),
+	POSTGRES_HOST: z.optional(z.string()),
+	POSTGRES_USER: z.optional(z.string()),
+});
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends z.infer<typeof envSchema> {}
+	}
+}
+
+const getEnv = () => envSchema.parse(process.env);
+
+export const env = getEnv();
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,19 +3,20 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { env } from "@/app/types/env";
 
 export const authOptions: NextAuthOptions = {
 	providers: [
 		GithubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID as string,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET,
 		}),
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
-	secret: process.env.NEXTAUTH_SECRET,
+	secret: env.NEXTAUTH_SECRET,
 	adapter: PrismaAdapter(prisma),
 	pages: {
 		signIn: "/login",
