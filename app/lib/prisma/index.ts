7b201a2ff2a6a84f779704a36b1d7aa11e16eb51
Enrique Santos <santosenrique869@gmail.com>
2023-04-24 19:05:51 -0300

    refactor: create new prisma client only serve-side context

--- app/lib/prisma/index.ts
+++ app/lib/prisma/index.ts
@@ -1,13 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+declare global {
+	// eslint-disable-next-line no-unused-vars
+	var prisma: PrismaClient | undefined;
+}
 
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient;
 
-export const prisma =
-	globalForPrisma.prisma ??
-	new PrismaClient({
-		log: ["query"],
-	});
+if (typeof window === "undefined") {
+	if (process.env.NODE_ENV === "production") {
+		prisma = new PrismaClient();
+	} else {
+		if (!global.prisma) {
+			global.prisma = new PrismaClient();
+		}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+		prisma = global.prisma;
+	}
+}
+//@ts-ignore
+export { prisma };
