6427f24f3cca6cc86a0152575924da27e510ffbd
Enrique Santos <santosenrique869@gmail.com>
2023-04-24 18:30:58 -0300

    refactor: update prisma client instance

--- app/lib/prisma/index.ts
+++ app/lib/prisma/index.ts
@@ -1,11 +1,13 @@
-/* eslint-disable no-unused-vars */
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-	var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+	prisma: PrismaClient | undefined;
+};
 
-const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export const prisma =
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		log: ["query"],
+	});
 
-export { prisma };
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
